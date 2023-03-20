9dd3884f3deb2219f91675c99054d82eb282819e
MDaud32 <theprofessor543@gmail.com>
2023-03-20 16:28:54 +0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Navbar from "./components/Navbar.jsx";
 import MainContent from "./components/MainContent.jsx";
 import Banner from "./components/Banner.jsx";
 import Pricing from "./components/Pricing.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-
   return (
-      <div>
-          <Navbar/>
-          <Hero/>
-          <MainContent/>
-          <Banner/>
-          <Pricing/>
-      </div>
-
-)
+    <div>
+      <Navbar />
+      <Hero />
+      <MainContent />
+      <Banner />
+      <Pricing />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
