7ccc2a75366c3e8702568900393595110016324f
MDaud32 <theprofessor543@gmail.com>
2023-03-21 15:16:50 +0500

    faq section

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,24 +1,71 @@
 import React, { useState } from "react";
 import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
+import "../index.css";
 
 const Faq = () => {
-  const [expand, setExpand] = useState(false);
+  const [expand, setExpand] = useState(null);
   const expandClass = expand ? "display" : "hidden";
   const ansClass = `${expandClass} p-4`;
+
+  const toggle = (i) => {
+    if (expand === i) {
+      return setExpand(null);
+    }
+    setExpand(i);
+  };
   return (
-    <div className="shadow rounded border border-gray-100 border-t-0 max-w-lg bg-gray-200 my-10 mx-auto">
-      <div className="p-4 text-xl relative font-medium">
-        <div className="w-5/6">are you a cat?</div>
-        <button
-          aria-label="question-expander"
-          className="text-xl absolute top-0 right-0 p-4 focus:outline-none"
-          onClick={() => setExpand(!expand)}>
-          {expand ? <RiArrowDropUpLine /> : <RiArrowDropDownLine />}
-        </button>
-      </div>
-      <div className={ansClass}>i am not a cat</div>
+    <div className="shadow rounded border border-gray-100 border-t-0 max-w-2xl bg-gray-200 my-10 mx-auto">
+      {data.map((item, i) => (
+        <>
+          <div key={i} className="p-4 text-md relative font-medium">
+            <div className="w-5/6 cursor-pointer" onClick={() => toggle(i)}>
+              {item.que}
+            </div>
+            <button
+              aria-label="question-expander"
+              className="text-xl absolute top-0 right-0 p-4 focus:outline-none"
+              onClick={() => toggle(i)}>
+              {expand === i ? <RiArrowDropUpLine /> : <RiArrowDropDownLine />}
+            </button>
+          </div>
+          <div className={`mx-4 ${expand === i ? "content show" : "content"}`}>
+            {item.ans}
+          </div>
+        </>
+      ))}
     </div>
   );
 };
 
 export default Faq;
+
+const data = [
+  {
+    que: "How do you see user identity as an admin?",
+    ans: "Is true that the bot has access to the user's information, but as described in the Terms of Service, the bot uses but do not store these inforations.",
+  },
+  {
+    que: "How do I delete my anonymous bot message from the channel?",
+    ans: "This bot does not has features for removing the messages, not even for the real sender.Check Slack's help page about deleting messages.",
+  },
+  {
+    que: "How can I disable the Anonymous bot?",
+    ans: "To disabled the bot in a single channel: remove the bot from the channel /kick @anonymous.To completely disable the bot, you need to remove the app. Check Slacks help page.",
+  },
+  {
+    que: "Is it free or not?",
+    ans: "Both. You and your team can use it for free. The bot also has addicional paid features (see Pricing section).",
+  },
+  {
+    que: "Is it open source?",
+    ans: "Not anymore. The first version of the bot was open source. You can still check its source here, but it has just the basic feature.",
+  },
+  {
+    que: "I'm not in this chat yet",
+    ans: "Slack says: I'm not in this chat yet. Please, invite me first. (The invite will be public)Before using Anonymous Bot you (or someone else) needs to invite the bot to the channel.Inviting bots or people is a public action: Slack automatically sends a message like Anonymous Bot was added to #general by username.",
+  },
+  {
+    que: "You are not allowed to send anonymous messages.",
+    ans: "The Team Plan has a feature to restrict access for some users.If you got this message, it means you are in the black list (or not in the white list).Only your team admin can change this.",
+  },
+];
